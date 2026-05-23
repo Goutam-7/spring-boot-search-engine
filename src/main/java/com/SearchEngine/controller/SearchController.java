c795706e240b8d7f92aafefc14d065d873924e18
Goutam <goutamcoolmsd7@gmail.com>
2026-05-23 12:12:43 +0530

    first commit

---
+++ src/main/java/com/SearchEngine/controller/SearchController.java
@@ -0,0 +1,35 @@
+package com.SearchEngine.controller;
+
+import com.SearchEngine.SearchEngineApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SearchController {
+
+ 
+
+	@RequestMapping("/home")
+	public String home()
+	{	
+		System.out.println("Home url triggered");
+		return "home";
+	}
+	
+	
+	@RequestMapping("/search")
+	public RedirectView search(@RequestParam("querybox") String query)
+	{
+		String url = "https://www.google.com/search?q=" + query ;
+		
+		RedirectView view = new RedirectView();
+		view.setUrl(url);
+		
+		return view ;
+		
+	}
+	
+	
+}
